b4c07378b04cb838308858b79c3ad86dd86628fc
root <tramboos123@gmail.com>
2020-10-19 23:39:25 +0530

    Started working on a shopping project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simple a test',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chicken-fajitas-horizontal-jpg-1522721531.jpg'),
+    new Recipe('A Test Recipe', 'This is simple a test',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chicken-fajitas-horizontal-jpg-1522721531.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
